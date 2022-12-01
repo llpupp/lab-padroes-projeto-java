42f7732ce6e87798563d894cd14f3e4517a49450
llpupp <llpupp@hotmail.com>
2022-12-01 09:13:29 -0300

    Comportamento Normal do Robo

--- src/one/digitalinnovation/gof/strategy/ComportamentoNormal.java
+++ src/one/digitalinnovation/gof/strategy/ComportamentoNormal.java
@@ -1,2 +1,10 @@
-package one.digitalinnovation.gof.strategy;public class ComportamentoNormal {
+package one.digitalinnovation.gof.strategy;
+
+public class ComportamentoNormal implements Comportamento {
+
+    @Override
+    public void mover() {
+        System.out.println("Movendo-se normalmente...");
+    }
 }
+
